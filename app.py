e2dde2ffbb3ce2395952b4300199f987d4eef0a5
Arya Pulkit <aryapulkit007@gmail.com>
2020-03-19 00:29:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,8 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Mar 16 17:03:17 2020
-
-@author: windows 10
-"""
-
 import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
-app=Flask(__name__)
 
+app = Flask(__name__)
 model = pickle.load(open('model.pkl', 'rb'))
 
 @app.route('/')
@@ -21,29 +14,13 @@ def predict():
     '''
     For rendering results on HTML GUI
     '''
-    print(request.form)
-   
     int_features = [int(x) for x in request.form.values()]
-    final_features = [np.array(int_features)]
-    
-    print(int_features)
-    print(final_features)
-    
+    final_features = [np.array(int_features)] 
     prediction = model.predict(final_features)
 
     output = round(prediction[0], 4)
 
     return render_template('index.html', prediction_text='Achieved Sales Value is  $ {}'.format(output))
-
-@app.route('/predict_api',methods=['POST'])
-def predict_api():
-    
-    data = request.get_json(force=True)
-    prediction = model.predict([np.array(list(data.values()))])
-
-    output = prediction[0]
-    return jsonify(output)
-
 
 if __name__ == "__main__":
     app.run(debug=True)
